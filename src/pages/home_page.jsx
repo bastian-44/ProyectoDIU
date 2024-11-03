69d907c613f358c84504c502c92ebcc72a1aeca0
bastian-44 <bastian.arismendi@usm.cl>
2024-11-03 15:20:54 -0300

    Menu casi listo

--- src/pages/home_page.jsx
+++ src/pages/home_page.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import Acordeon from '../components/Acordeon';
+import React from 'react'
+import Acordeon from '../components/Acordeon'
+import { NavLink } from 'react-router-dom'
 
 function HomePage() {
   return (
@@ -11,19 +12,53 @@ function HomePage() {
       </Acordeon>
 
       <Acordeon title="Fechas">
-        <p>React se usa componiendo componentes, que representan diferentes partes de la interfaz de usuario.</p>
+        <p>- Control 1: xx/xx</p>
+        <p>- Certamen 1: xx/xx</p>
+        <p>- Control 2: xx/xx</p>
+        <p>- Certamen 2: xx/xx</p>
+        <p>- Control 3: xx/xx</p>
+        <p>- Certamen 3: xx/xx</p>
       </Acordeon>
 
       <Acordeon title="Entregas">
-        <p>React es popular por su facilidad para crear aplicaciones dinámicas y su eficiente renderizado.</p>
+        <p><NavLink to="/lightbulb" className="navlink">Entrega 1</NavLink></p>
+        <p><NavLink to="/lightbulb" className="navlink">Entrega 2</NavLink></p>
+        <p><NavLink to="/lightbulb" className="navlink">Entrega 3</NavLink></p>
       </Acordeon>
 
       <Acordeon title="Notas">
-        <p>React es popular por su facilidad para crear aplicaciones dinámicas y su eficiente renderizado.</p>
+        <p>- Control 1</p>
+        <ul>
+          <li>Notas-Q1.xlsx</li>
+        </ul>
+        <p>- Certamen 1</p>
+        <ul>
+          <li>Notas-C1.xlsx</li>
+        </ul>
       </Acordeon>
 
       <Acordeon title="Material">
-        <p>React es popular por su facilidad para crear aplicaciones dinámicas y su eficiente renderizado.</p>
+        <Acordeon title="Certamen 1">
+          <ul>
+            <li>Material 1</li>
+            <li>Material 2</li>
+            <li>Material 3</li>
+          </ul>
+        </Acordeon>
+        <Acordeon title="Certamen 2">
+          <ul>
+            <li>Material 1</li>
+            <li>Material 2</li>
+            <li>Material 3</li>
+          </ul>
+        </Acordeon>
+        <Acordeon title="Certamen 3">
+          <ul>
+            <li>Material 1</li>
+            <li>Material 2</li>
+            <li>Material 3</li>
+          </ul>
+        </Acordeon>
       </Acordeon>
     </div>
   );
